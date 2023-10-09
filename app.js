311dbf6d46c7a4899e839552e79aec3c1cafc9fd
Neldrom <nikimev01@gmail.com>
2023-10-09 18:36:27 +0300

    -m "ses"

--- app.js
+++ app.js
@@ -9,28 +9,36 @@ const { SESSION_SECRET, IS_PRODUCTION, MONGODB_URI } = require('./configs');
 const app = express();
 
 const store = new MongoDBStore({
-  uri: MONGODB_URI,
-  collection: 'sessions',
+    uri: MONGODB_URI,
+    collection: 'sessions',
+});
+
+store.on('error', (error) => {
+    console.error(`Games DB Connection Error: ${error.message}`);
+});
+
+store.once('open', () => {
+    console.log('Games DB Connected');
 });
 
 app.use(express.json({ limit: '1KB' }));
 
 app.use(
-  expressSession({
-    name: 'dron',
-    resave: false,
-    saveUninitialized: false,
-    secret: SESSION_SECRET,
-    cookie: {
-      secure: IS_PRODUCTION,
-      maxAge: 1000 * 60 * 60 * 24,
-    },
-    store: store,
-  })
+    expressSession({
+        name: 'dron',
+        resave: false,
+        saveUninitialized: false,
+        secret: SESSION_SECRET,
+        cookie: {
+            secure: IS_PRODUCTION,
+            maxAge: 1000 * 60 * 60 * 24,
+        },
+        store: store,
+    })
 );
 app.use(cors({
     origin: 'https://neldrom.github.io',
-  }));
+}));
 
 app.use('/api/v1/auth', authRoutes)
 
