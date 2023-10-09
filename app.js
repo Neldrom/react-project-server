ac3a2d6a0ff60ba3eb00f85e0b03b690b574ab59
Neldrom <nikimev01@gmail.com>
2023-10-09 20:41:06 +0300

    1

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const app = express();
 
 
 app.use(express.json({limit: '1KB'}))
-app.use(session({
-    store: MongoStore.create({
-      mongoUrl: process.env.GAME_URI,
-      ttl: 14 * 24 * 60 * 60
-    })
-  }));
+// app.use(session({
+//     store: MongoStore.create({
+//       mongoUrl: process.env.MONGODB_URI,
+//       ttl: 14 * 24 * 60 * 60
+//     })
+//   }));
 
 app.use(cors({
     origin: 'https://neldrom.github.io',
