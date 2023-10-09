9c8565198b4ee1ce24425e8b5c6aabab7e6d49c4
Neldrom <nikimev01@gmail.com>
2023-10-09 19:11:45 +0300

    new store

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 const express = require('express');
 const expressSession = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(expressSession);
+const mongoose = require('mongoose');
 const cors = require('cors')
 
 const authRoutes = require('./routes/auth');
-const {SESSION_SECRET, IS_PRODUCTION} =require('./configs');
+const { SESSION_SECRET, MONGODB_URI } = require('./configs');
 
 const app = express();
+const sessionStore = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions',
+    mongooseConnection: mongoose.createConnection(MONGODB_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    }),
+});
 
+app.use(express.json({ limit: '1KB' }));
 
-app.use(express.json({limit: '1KB'}))
 app.use(
     expressSession({
-        name: "dron",
+        store: sessionStore,
+        secret: SESSION_SECRET,
         resave: false,
         saveUninitialized: false,
-        secret: SESSION_SECRET,
-        cookie:{
-            secure: IS_PRODUCTION,
+        cookie: {
+            secure: true,
             maxAge: 1000 * 60 * 60 * 24,
-        }
+        },
     })
-)
+);
+
 app.use(cors({
     origin: 'https://neldrom.github.io',
-  }));
+}));
 
 app.use('/api/v1/auth', authRoutes)
 
