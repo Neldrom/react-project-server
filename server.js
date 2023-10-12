cc1ce3ed1440644d36bb595c7d6a0a3386a189a4
Neldrom <nikimev01@gmail.com>
2023-10-12 10:54:18 +0300

    1

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
-
-const app = require("./app")
 const { PORT } = require('./configs');
+const express = require('express');
+const expressSession = require('express-session');
+const MongoStore = require("connect-mongo");
+const cors = require('cors')
 
+const authRoutes = require('./routes/auth');
+const { SESSION_SECRET } = require('./configs');
+
+const app = express();
+
+app.use(expressSession({
+  secret: SESSION_SECRET,
+  saveUninitialized: false, 
+  resave: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI,
+    touchAfter: 24 * 3600 
+  })
+}));
+app.use(cors({
+  origin: 'https://neldrom.github.io',
+}));
+
+app.use('/api/v1/auth', authRoutes)
 
 app.listen(PORT, () => {
     console.log(`server started listening on port ${PORT}...`)
