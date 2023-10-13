761fd373ac7270051e55536bbc44872ecb80d75a
Neldrom <120821369+Neldrom@users.noreply.github.com>
2023-10-13 17:21:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.set("trust proxy", 1);
 app.use(express.json({limit: '1KB'}))
 
-app.use(expressSession(
+app.use(expressSession({
     secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
